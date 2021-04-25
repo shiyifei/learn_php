2880a442a1ae6deb0d6e2b581738cacb3d8ced84
shiyifei <shi_yi_fei@163.com>
2021-04-26 01:13:05 +0800

    autoload

--- spl_object/old_register.php
+++ spl_object/old_register.php
@@ -6,31 +6,51 @@
 //测试 session_start()放在加载器的前面的写法
 session_start();
 
+$ret = get_included_files();
+echo '111,before __autoload(), included files:'.var_export($ret, true).'<hr/>';
+
+$ret = class_exists('ModelC');
+echo '111, after executing class_exists("ModelC"), class ModelC is exists:'.var_export($ret, true).'<hr/>';
+
+$ret = get_included_files();
+echo '112,after executing class_exists("ModelC"), included files:'.var_export($ret, true).'<hr/>';
+
+//$ret = new ReflectionClass('ModelA');
+$ret = class_exists('ModelA');
+echo '111,after executing class_exists("ModelA"), class ModelA is exists:'.var_export($ret, true).'<hr/>';
+
+$ret = get_included_files();
+echo '113,after executing class_exists("ModelC"), included files:'.var_export($ret, true).'<hr/>';
+
 /**
  * 按照类名加载相关类
  * @param $className
  */
 function __autoload($className)
 {
+    echo "I am in ".__FUNCTION__."<br/>";
     include('model/' . $className . '.php');
     include('logic/' . $className . '.php');
 }
 
+$ret = get_included_files();
+echo '222,after __autoload(), included files:'.var_export($ret, true).'<hr/>';
+
 $ret = class_exists('ModelA');
-echo 'before __autoload(), class ModelA is exists:'.var_export($ret, true).'<hr/>';
+echo '222,after __autoload(), class ModelA is exists:'.var_export($ret, true).'<hr/>';
 
 
 $objA = new ModelA();
 
 $ret = class_exists('ModelA');
-echo 'after __autoload(), class ModelA is exists:'.var_export($ret, true).'<hr/>';
+echo 'after new ModelA(), class ModelA is exists:'.var_export($ret, true).'<hr/>';
 
 
 $ret = class_exists('LogicA');
-echo 'before __autoload(), class LogicA is exists:'.var_export($ret, true).'<hr/>';
+echo 'before new LoginA(), class LogicA is exists:'.var_export($ret, true).'<hr/>';
 
 
 $objA = new LogicA();
 
 $ret = class_exists('LogicA');
-echo 'after __autoload(), class LogicA is exists:'.var_export($ret, true).'<hr/>';
+echo 'after new LogicA(), class LogicA is exists:'.var_export($ret, true).'<hr/>';
